a3605c163f3febe22b34cf7e502ae83ff6577a53
LeoFM93 <120279873+LeoFM93@users.noreply.github.com>
2023-01-14 14:12:36 -0500

    Delete R directory

--- R/Fundamentos R/Covarianza.R
+++
@@ -1,7 +0,0 @@
-#ESTADÍSTICA
-data <- data.frame (Appearance = c(8, 8, 8, 9, 7, 9, 9, 7, 8, 9),
-                    Thickness = c(8, 8, 7, 7, 7, 8, 9, 8, 7, 9),
-                    Spredability = c(7, 9, 9, 9, 8, 8, 7, 8, 6, 7))
-data
-cov(data)
-
